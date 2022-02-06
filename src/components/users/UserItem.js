529b6fe8b685198a2d739f7d0199142f2717487f
oani <callmeoani@gmail.com>
2022-02-06 03:20:49 +0100

    created the navbar, users component,useritem component and the spinner component

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserItem = ({user: { login, html_url, avatar_url }}) => {
+
+    return(
+        <div className='card text-center'>
+        <img 
+            src={avatar_url} 
+            alt="avatar" 
+            className='round-image' 
+            style={{width: '60px'}}
+        />
+        <h3>{login}</h3>
+
+        <div>
+            <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+        </div>
+
+    </div>
+    )
+   
+}
+
+UserItem.propTypes ={
+    user: PropTypes.object.isRequired,
+
+}
+
+export default UserItem;
